9531fda4d880b19b5ac18adacb22e3ccb3a4542f
Xwilarg <xwilarg@protonmail.com>
2024-05-15 15:41:01 +0200

    Add image import

--- Images-Classifier/ViewModels/MainViewModel.cs
+++ Images-Classifier/ViewModels/MainViewModel.cs
@@ -1,6 +1,42 @@
-﻿namespace Images_Classifier.ViewModels;
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform.Storage;
+using ReactiveUI;
+using System.Linq;
+using System.Windows.Input;
+
+namespace Images_Classifier.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public MainViewModel()
+    {
+        ImportImageCmd = ReactiveCommand.CreateFromTask(async () =>
+        {
+            var mainWindow = Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop ? desktop.MainWindow : null;
+
+            var files = await mainWindow.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open Image",
+                AllowMultiple = false,
+                FileTypeFilter = [ FilePickerFileTypes.ImageAll ]
+            });
+
+            if (files.Any())
+            {
+                Source = new Bitmap(files.ElementAt(0).Path.AbsolutePath);
+            }
+        });
+    }
+
+    private IImage _source;
+    public IImage Source
+    {
+        get => _source;
+        set => this.RaiseAndSetIfChanged(ref _source, value);
+    }
+
+    public ICommand ImportImageCmd { get; }
 }
