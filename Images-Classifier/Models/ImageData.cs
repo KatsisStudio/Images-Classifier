2a19e4f8d16bfb1334d790e8903d7b9066652c49
Xwilarg <xwilarg@protonmail.com>
2024-05-15 21:42:11 +0200

    Add firsts data filling

--- Images-Classifier/Models/ImageData.cs
+++ Images-Classifier/Models/ImageData.cs
@@ -1,11 +1,80 @@
-﻿namespace Images_Classifier.Models
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Images_Classifier.Models
 {
     public class ImageData
     {
-        public string ImageId { set; get; }
+        [JsonPropertyName("id")]
+        public string Id { set; get; }
 
+        [JsonPropertyName("format")]
+        public string Format { set; get; }
+
+        [JsonPropertyName("parent")]
+        public string Parent { set; get; }
+
+        [JsonPropertyName("author")]
         public string Author { set; get; }
 
-        public string Tags { set; get; }
+        [JsonPropertyName("rating")]
+        public int Rating { set; get; }
+
+        [JsonPropertyName("text")]
+        public Text Tag { set; get; }
+
+        [JsonPropertyName("tags")]
+        public Tag Tags { set; get; }
+
+        [JsonPropertyName("comment")]
+        public string Comment { set; get; }
+    }
+
+    public class Tag
+    {
+        [JsonPropertyName("parody")]
+        public string[] Parody { set; get; }
+
+        [JsonPropertyName("characters")]
+        public Character Characters { set; get; }
+
+        [JsonPropertyName("poses")]
+        public string[] Poses { set; get; }
+
+        [JsonPropertyName("clothes")]
+        public string[] Clothes { set; get; }
+
+        [JsonPropertyName("sexes")]
+        public string[] Sexes { set; get; }
+
+        [JsonPropertyName("others")]
+        public string[] Others { set; get; }
+    }
+
+    public class Character
+    {
+        [JsonPropertyName("sexes")]
+        public Dictionary<string, int> Sex { set; get; }
+
+        [JsonPropertyName("races")]
+        public Dictionary<string, int> Races { set; get; }
+
+        [JsonPropertyName("names")]
+        public string[] Names { set; get; }
+
+        [JsonPropertyName("racial_attributes")]
+        public string[] RacialAttributes { set; get; }
+
+        [JsonPropertyName("attributes")]
+        public string[] Attributes { set; get; }
+    }
+
+    public class Text
+    {
+        [JsonPropertyName("lang")]
+        public string Language { set; get; }
+
+        [JsonPropertyName("content")]
+        public string[] Content { set; get; }
     }
 }
--- Images-Classifier/ViewModels/MainViewModel.cs
+++ Images-Classifier/ViewModels/MainViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform.Storage;
+using DynamicData;
+using Images_Classifier.Models;
 using ReactiveUI;
 using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Windows.Input;
 
 namespace Images_Classifier.ViewModels;
@@ -16,9 +21,46 @@ public class MainViewModel : ViewModelBase
 {
     public MainViewModel()
     {
-        if (!Directory.Exists("export")) Directory.CreateDirectory("export");
-        if (!Directory.Exists("export/images")) Directory.CreateDirectory("export/images");
-        if (!Directory.Exists("export/thumbnails")) Directory.CreateDirectory("export/thumbnails");
+        if (!Design.IsDesignMode)
+        {
+            if (!Directory.Exists("export")) Directory.CreateDirectory("export");
+            if (!Directory.Exists("export/images")) Directory.CreateDirectory("export/images");
+            if (!Directory.Exists("export/thumbnails")) Directory.CreateDirectory("export/thumbnails");
+        }
+
+        ImportMetadataCmd = ReactiveCommand.CreateFromTask(async () =>
+        {
+            var mainWindow = Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop ? desktop.MainWindow : null;
+
+            var files = await mainWindow.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open Metadata File",
+                AllowMultiple = false,
+                FileTypeFilter = [
+                    new FilePickerFileType("JSON file")
+                    {
+                        Patterns = [ "*.json" ],
+                        MimeTypes = [ "application/json" ]
+                    }
+                ]
+            });
+
+            if (files.Any())
+            {
+                try
+                {
+                    Metadatas = JsonSerializer.Deserialize<ImageData[]>(File.ReadAllText(files.ElementAt(0).Path.AbsolutePath));
+
+                    ParentChoices.AddRange(Metadatas.Select(x => x.Id));
+                    AuthorChoices.AddRange(Metadatas.Select(x => x.Author));
+                }
+                catch (Exception ex)
+                {
+                    if (Debugger.IsAttached)
+                        Debugger.Break();
+                }
+            }
+        });
 
         ImportImageCmd = ReactiveCommand.CreateFromTask(async () =>
         {
@@ -76,6 +118,13 @@ public class MainViewModel : ViewModelBase
 
     private string _extension;
 
+    private ImageData[] _metadatas;
+    public ImageData[] Metadatas
+    {
+        get => _metadatas;
+        set => this.RaiseAndSetIfChanged(ref _metadatas, value);
+    }
+
     private IImage _source;
     public IImage Source
     {
@@ -84,6 +133,33 @@ public class MainViewModel : ViewModelBase
     }
 
     public ICommand ImportImageCmd { get; }
+    public ICommand ImportMetadataCmd { get; }
     public ICommand Cancel { get; }
     public ICommand Save { get; }
+
+    private string _parentText;
+    public string ParentText
+    {
+        get => _parentText;
+        set => this.RaiseAndSetIfChanged(ref _parentText, value);
+    }
+    public ObservableCollection<string> ParentChoices { private set; get; } = [];
+
+    private string _authorText;
+    public string AuthorText
+    {
+        get => _authorText;
+        set => this.RaiseAndSetIfChanged(ref _authorText, value);
+    }
+    public ObservableCollection<string> AuthorChoices { private set; get; } = [];
+
+    private int _ratingIndex;
+    public int RatingIndex
+    {
+        get => _ratingIndex;
+        set => this.RaiseAndSetIfChanged(ref _ratingIndex, value);
+    }
+    public ObservableCollection<string> RatingChoices { private set; get; } = [
+        "Safe", "Questionnable", "Explicit"
+    ];
 }
